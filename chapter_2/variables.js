28852025d910bd5313f26688b976ec9e3af31b75
Jhordan Tejada <jhordantej@gmail.com>
2024-01-04 18:55:46 -0600

    Variable.js
    Added examples and explanations for using 'let', 'const', and 'var' to declare variables in JavaScript.

---
+++ chapter_2/variables.js
@@ -0,0 +1,21 @@
+// Using let is the modern way to declare variables.
+let age = 25;
+let year = 2023;
+console.log(age, year);
+
+// Reassigning the age variable to a new value.
+age = 30;
+console.log(age);
+
+// Using const is the modern way to declare variables that cannot change its value.
+const points = 100;
+// Trying to reassign the points variable to a new value.
+points = 90;
+// This should cause an error.
+console.log(points);
+
+// Using var is the old way to declare variables.
+var score = 75;
+console.log(score);
+
+// Go more into detail later about the difference between the let and var keywords later on in the course.
