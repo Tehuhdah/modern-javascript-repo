b961f63216d447024e6bc87dbacc08d86cef6904
Jhordan Tejada <jhordantej@gmail.com>
2024-01-04 23:54:38 -0600

    Numbers.js
    Overview on Mathematical Operations in JavaScript.

---
+++ chapter_2/numbers.js
@@ -0,0 +1,44 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// Math Operators +, -, *, /, **, %
+//console.log(10 / 2);
+//console.log(10 + 10);
+//console.log(10 - 5);
+//console.log(2 * 4);
+//console.log(4 ** 2);
+
+// % - Modulus, returns the remainder of a division equation.
+// let result = radius % 3;
+// let result = pi * radius ** 2; // 3.14 * 10 ^ 2
+
+// Order of Operation - B I D M A S
+// Brackets, Idices, Division, Multiplication, Addition, Submission
+
+// let result = 5 * (10 - 3) ** 2; // 5 * 49
+
+// console.log(result);
+
+let likes = 10;
+
+// Shorthand notations
+// likes = likes + 1; // Longer way
+// likes++; // Shorter way, +1
+// likes--; // -1
+
+// likes += 10; // Shorthand way to add to existing variables.
+// likes -= 5; // Shorthand way to subtract from existing variables.
+// likes *= 2; // Shorthand way to multiply from existing variables.
+// likes /= 2; // Shorthand way to divide from existing variables.
+
+// console.log(likes);
+
+// NaN - Not a Number
+// console.log(5 / "hello"); // Outputs NaN
+// console.log(5 * "hello"); // Outputs NaN
+
+// You can do string concatenation with numbers.
+let result = "the blog has " + likes + " likes";
+console.log(result);
