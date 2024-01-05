1d93637cea353374c90ecdb163dd8ffbfcc130a9
Jhordan Tejada <jhordantej@gmail.com>
2024-01-05 13:51:25 -0600

    Methods&Functions.js
    Understand the difference between a method and function in JavaScript.

---
+++ chapter_4/methods&functions.js
@@ -0,0 +1,17 @@
+// 27:00
+
+const name = "Jhordan";
+
+// Functions
+// A function is a block of code designed to perform a particular task.
+// A function is executed when something invokes it.
+const greet = () => "Hello";
+
+let resultOne = greet();
+console.log(resultOne);
+
+// Methods
+// A method in JavaScript is a function that is a property of an object.
+// Here, toUpperCase() is a method of the String object 'name'.
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
