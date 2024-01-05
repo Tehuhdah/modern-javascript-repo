f23199eacda30d0ed08cf96f2c9fd3eb4a1f0e8e
Jhordan Tejada <jhordantej@gmail.com>
2024-01-05 10:11:48 -0600

    Conditional_Statements.js
    Examples of if statements in JavaScript.

---
+++ chapter_3/conditional_statements.js
@@ -0,0 +1,22 @@
+// 17:30
+
+// If Statements
+const age = 25;
+
+// If condition is met, the code block below will run.
+if (age > 20) {
+  console.log("You are over 20 years old");
+}
+
+const ninjas = ["Jhordan", "Mario", "Luigi", "Yoshi"];
+
+// Example using arrays.
+if (ninjas.length > 3) {
+  console.log("That's a lot of ninjas.");
+}
+
+const password = "password";
+// Password Example
+if (password.length >= 8) {
+  console.log("That password is long enough!");
+}
