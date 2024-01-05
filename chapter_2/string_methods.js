5291bf5d80d822fe9e04945429c63d46bfbc6eb9
Jhordan Tejada <jhordantej@gmail.com>
2024-01-04 23:20:10 -0600

    String_Methods.js
    Examples of different string methods in JavaScript.

---
+++ chapter_2/string_methods.js
@@ -0,0 +1,27 @@
+// 31:00
+
+// Common String Methods
+let email = "jhordantej@gmail.com";
+
+// Returning the last index that a appears in the string.
+// let result = email.lastIndexOf("a");
+
+// Returning the string when sliced from the start position, to the end position.
+// let result = email.slice(0, 5); // starts from position 0, and then extracts to position 5.
+
+// Returning the substring from the start position, to the length which represents how many characters
+// you wish to extract.
+// let result = email.substr(4, 10); // Starts at position 4, and then extracts 10 characters.
+
+// Example using negative index
+// when using a negative, it starts from the position from the end of the string going backwards.
+
+// Starts at -5 position which is l, then continues to the end of the string.
+// console.log(email.slice(-5)); // Outputs: "l.com"
+// Starts at -5 position which is l, then continues until it extracts 4 characters.
+// console.log(email.substr(-5, 4)); // Outputs: "l.co"
+
+// Replaces the character specified in the first argument, with the character in the second argument.
+// let result = email.replace("a", "$");
+
+console.log(result);
