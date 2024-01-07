e65cc616c8384f10cd2cafe77c33e9f7dc51bf13
Jhordan Tejada <jhordantej@gmail.com>
2024-01-06 18:15:52 -0600

    Dom_Intro.js
    Some information about the Document Object Model in JavaScript.

---
+++ chapter_6/dom_intro.js
@@ -0,0 +1,5 @@
+// Typing 'document' in the console returns the Document Object Model (DOM) of the current webpage.
+// The 'document' object is a built-in JavaScript object that represents the webpage and allows you
+// to manipulate its content and structure.
+
+// You can run some methods on the document to get information about your html page.
